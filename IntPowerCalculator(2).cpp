b73a1f0dd2a1034c14062ea7c7e2705789b5b462
Dos Acker <117856724+Dacker3@users.noreply.github.com>
2022-11-09 14:49:30 -0600

    Add files via upload

--- IntPowerCalculator(2).cpp
+++ IntPowerCalculator(2).cpp
@@ -1,15 +1,45 @@
 //============================================================================
 // Name        : IntPowerCalculator(2).cpp
-// Author      : Dos
+// Author      : Dosreis Acker
+// Date        : 5/7/2022
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Instructor  : Dr. Duncan
+// Description : Caculates the power of an integer
+// File        : IntPowerCaculator.cpp
+// </pre>
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
+
+
 using namespace std;
 
-int main() {
-	cout << "" << endl; // prints 
-	return 0;
+int main()
+{
+    double base, power;
+    int exp, i;
+    //Read data
+    cout << "Enter the base and integer exponent of the power -> ";
+    cin >> base >> exp;
+    if(base == 0.0)
+    { // If base is 0
+        if(exp <= 0) // If n <= 0 then indeterminate
+            cout << base << "^" << exp << " = nan";
+        else // else 0
+            cout << base << "^" << exp << " = 0.00000";
+    }
+    else{
+        power = 1.0;
+        if(exp >= 0){ // If n >= 0
+            for(i = 1; i <= exp; i++)
+                power *= base; // Calculate and display
+            cout << base << "^" << exp << " = " << fixed << setprecision(5) << power ;
+        }
+        else{
+            for(i = 1; i <= 0 - exp; i++)
+                power *= base;
+            cout << base << "^" << exp << " = " << fixed << setprecision(5) << 1.0 / power ;
+        }
+    }
 }
